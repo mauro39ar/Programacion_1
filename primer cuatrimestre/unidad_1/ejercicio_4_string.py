950442d3edadd1bf0b4b96ad565c0fbe8e543cd9
mauro39ar <mauro37ar@gmail.com>
2017-09-15 12:20:58 -0300

    subiendo ejercicio_4

---
+++ primer cuatrimestre/unidad_1/ejercicio_4_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+sakura="en python es posible armar un parrafo muy sencillamente"
+
+card=sakura[3:13]
+
+captor=sakura[-14:]
+
+darien=sakura[-18:-14]
+
+selena=sakura[13:21]
+
+print  card + captor + darien  + selena
