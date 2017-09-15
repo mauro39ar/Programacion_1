c41c42ebc848c1af41e27c7effce9ea2ebffc4e7
mauro39ar <mauro37ar@gmail.com>
2017-09-15 12:44:57 -0300

    corrigiendo strings y subiendo lista

---
+++ primer cuatrimestre/unidad_1/Listas/ejercicio_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+serena=raw_input("ingrese nombre: ")
+
+tsukino=raw_input("ingrese apellido: ")
+
+amy= input("ingrese anio de nacimiento: ")
+
+mizuno=raw_input("ingrese lugar de nacimiento: ")
+
+lita= input("ingrese su dni: ")
+
+sailors=[serena, tsukino, amy, mizuno, lita, 0]
+
+print sailors
---
+++ primer cuatrimestre/unidad_1/Listas/ejercicio_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+serena=raw_input("ingrese nombre: ")
+
+tsukino=raw_input("ingrese apellido: ")
+
+amy= input("ingrese anio de nacimiento: ")
+
+mizuno=raw_input("ingrese lugar de nacimiento: ")
+
+lita= input("ingrese su dni: ")
+
+kumico= amy * lita
+
+sailors=[serena, tsukino, amy, mizuno, lita, kumico]
+
+print sailors
